46ddb0d06680a9b5fe17098f62307c75e8d5ae98
Sahan Kavinda <mdskav431@gmail.com>
2024-07-24 14:03:39 +0530

    aa

--- src/WeatherStation.java
+++ src/WeatherStation.java
@@ -8,6 +8,8 @@ public class WeatherStation {
         //ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
         weatherData.setMeasurements(80, 65, 30.4f);
         weatherData.setMeasurements(82, 70, 29.2f);
+
+
         weatherData.setMeasurements(78, 90, 29.2f);
     }
 }
